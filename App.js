52ac362b82e1cc93137c192b6112af44a54cb6d5
Hassan Abbas <hazzyhassan888@gmail.com>
2021-08-28 13:52:15 +0500

    extra

--- App.js
+++ App.js
@@ -3,14 +3,23 @@ import { View, Text, StatusBar } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import COLORS from "./src/common/colors";
 import Navigator from "./src/helpers/navigationHelper/navigationHelper";
+import SplashScreen from "react-native-splash-screen";
 
-function App() {
-  return (
-    <View style={{ flex: 1 }}>
-      <StatusBar animated={true} hidden={true} />
-      <Navigator />
-    </View>
-  );
+class App extends React.Component {
+  componentDidMount() {
+    // do stuff while splash screen is shown
+    // After having done stuff (such as async tasks) hide the splash screen
+    SplashScreen.hide();
+  }
+
+  render() {
+    return (
+      <View style={{ flex: 1 }}>
+        <StatusBar animated={true} hidden={true} />
+        <Navigator />
+      </View>
+    );
+  }
 }
 
 export default App;
